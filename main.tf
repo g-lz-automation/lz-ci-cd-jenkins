629420be811cff4d01fa8cc2d7f5e94ed49c7663
Prateek Khushalani <prateek.khushalani@gmail.com>
2022-02-15 03:56:50 +0000

    Adding Basic TF code for Jenkins GKE autopilot

---
+++ main.tf
@@ -0,0 +1,90 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+/*****************************************
+  Activate Services in Jenkins Project
+ *****************************************/
+module "enables-google-apis" {
+  source  = "terraform-google-modules/project-factory/google//modules/project_services"
+
+  project_id = module.jenkins-project.project_id
+
+  activate_apis = [
+    "iam.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "compute.googleapis.com",
+    "containerregistry.googleapis.com",
+    "container.googleapis.com",
+    "storage-component.googleapis.com",
+    "logging.googleapis.com",
+    "monitoring.googleapis.com",
+  ]
+}
+
+/*****************************************
+  Jenkins VPC
+ *****************************************/
+module "jenkins-vpc" {
+  source  = "terraform-google-modules/network/google"
+
+  project_id   = module.enables-google-apis.project_id
+  network_name = var.network_name
+
+  subnets = [
+    {
+      subnet_name   = var.subnet_name
+      subnet_ip     = "10.0.0.0/17"
+      subnet_region = var.region
+    },
+  ]
+
+  secondary_ranges = {
+    "${var.subnet_name}" = [
+      {
+        range_name    = var.ip_range_pods_name
+        ip_cidr_range = "192.168.0.0/18"
+      },
+      {
+        range_name    = var.ip_range_services_name
+        ip_cidr_range = "192.168.64.0/18"
+      },
+    ]
+  }
+}
+
+resource "google_container_cluster" "jenkins-gke" {
+  name                      = "jenkins"
+  enable_autopilot          = true
+  location                  = var.region
+  network                   = module.jenkins-vpc.network_name
+  subnetwork                = module.jenkins-vpc.subnets_names[0]
+  project                   = module.enables-google-apis.project_id
+  ip_allocation_policy {
+  }
+}
+
+/*****************************************
+  IAM Bindings GKE SVC
+ *****************************************/
+# allow GKE to pull images from GCR
+/*resource "google_project_iam_member" "gke" {
+  project = module.enables-google-apis.project_id
+  role    = "roles/storage.objectViewer"
+
+  member = "serviceAccount:${google_container_cluster.jenkins-gke.service_account}"
+}*/
+
---
+++ terraform.tfvars
@@ -0,0 +1,13 @@
+tfstate_gcs_backend = "hnb-bkt-tfstate-60c0"
+region = "asia-south2"
+zones = ["asia-south2-a"]
+ip_range_pods_name = "ip-range-pods"
+ip_range_services_name = "ip-range-scv"
+network_name = "jenkins-network"
+subnet_ip = "10.10.10.0/24"
+subnet_name = "jenkins-subnet"
+jenkins_k8s_config = "jenkins-k8s-config"
+
+github_repo = "automated-lz"
+github_token = "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"
+github_username = "prateek2408"
